74b3aecafd6c8c6da74e0cc88f097962cb2f1a73
yemiemy <rasholayemi@gmail.com>
2024-07-10 05:02:08 +0100

    updated backend api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
+export const API_BASE_URL = "https://3acb-102-89-41-52.ngrok-free.app/api/v1";
 
 export default axios.create({
     baseURL: API_BASE_URL,
